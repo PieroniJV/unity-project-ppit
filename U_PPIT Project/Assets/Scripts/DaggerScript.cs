0c6da03fae8b6fe1b3856e4ea1b84c9a408d360e
Joshua Uzell <G00391557@gmit.ie>
2023-03-11 11:29:12 +0000

    Updated player sprite and other changes
    Updated the player sprite with a new model and walking animations. Added a test scene for testing out close combat. Made other miscellaneous changes to different scripts and started adding sorting layers for different gameobjects.

--- U_PPIT Project/Assets/Scripts/DaggerScript.cs
+++ U_PPIT Project/Assets/Scripts/DaggerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,27 @@ public class DaggerScript : MonoBehaviour
     [SerializeField] private GameObject dagger;
     public float daggerSpeed;
     private Rigidbody2D spawnedDaggerRb;
+    private bool hasFired = false;
 
-    // Update is called once per frame
-    void Update()
+    //Update is used for input from player
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
+            hasFired = true;
+        }
+    }
+
+    //Fixed Update used for physics of dagger
+    void FixedUpdate()
+    {
+        if (hasFired)
+        {
             var spawnedDagger = Instantiate(dagger, transform.position, Quaternion.identity);
             spawnedDaggerRb = spawnedDagger.GetComponent<Rigidbody2D>();
-            spawnedDaggerRb.velocity = Vector2.up * (daggerSpeed * Time.deltaTime);
-            Destroy(spawnedDaggerRb, 4f);
+            spawnedDaggerRb.velocity = Vector2.up * (daggerSpeed * Time.fixedDeltaTime);
+            Destroy(spawnedDagger, 4f);
+            hasFired = false;
         }
-        
     }
 }
--- U_PPIT Project/Assets/Scripts/PlayerMovement.cs
+++ U_PPIT Project/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 10.0f;
     private Rigidbody2D rb2D;
+    private Vector2 movement;
+    private Animator playerAnimator;
 
-    private void Start()
+    private void Awake()
     {
+        playerAnimator = gameObject.GetComponentInChildren<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        
+        playerAnimator.SetFloat("Horizontal", movement.x );
+        playerAnimator.SetFloat("Vertical", movement.y);
+        playerAnimator.SetFloat("PlayerSpeed", movement.sqrMagnitude);
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            playerAnimator.SetFloat("LastHorizontal", movement.x);
+            playerAnimator.SetFloat("LastVertical", movement.y);
+        }
     }
 
     private void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector2 directionOfMovement = new Vector2(horizontalInput, verticalInput);
-
+        Vector2 directionOfMovement = new Vector2(movement.x, movement.y);
         rb2D.velocity = directionOfMovement * (movementSpeed * Time.fixedDeltaTime);
     }
 }
