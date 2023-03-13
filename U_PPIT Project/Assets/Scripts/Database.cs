c8c4da1a511629361b0edcc13a360395984a0577
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2023-03-13 14:22:27 +0000

    Database Script, New Scene
    Fixed database script, added it to new scene "LeaderBoard", added functionality to display dummy database results to leaderboard screen

--- U_PPIT Project/Assets/Scripts/Database.cs
+++ U_PPIT Project/Assets/Scripts/Database.cs
@@ -3,33 +3,77 @@ using System.Collections.Generic;
 using System.Data;
 using Mono.Data.Sqlite;
 using UnityEngine;
+using TMPro;
 
 public class Database : MonoBehaviour
 {
-    private string dbName;
+    private IDbConnection connection;
+    private IDbCommand command;
+    private IDataReader reader;
+    private string dbName = "myDatabase.db";
+    private string tableName = "game_stats";
+
+    private TMP_Text textMeshPro;
 
     private void Start()
     {
-        dbName = "URI=file:" + Application.dataPath + "/Database/ppitDB.db";
+        textMeshPro = GameObject.Find("Scores").GetComponent<TMP_Text>();
+        // Connection string for SQLite database
+        string connectionString = "URI=file:" + Application.dataPath + dbName;
+        // Create a database connection
+        connection = new SqliteConnection(connectionString);
+        // Open connection
+        connection.Open();
+        // Function to read data from database
         DisplayData();
+        // Close connection
+        connection.Close();
     }
 
     private void DisplayData()
     {
-        using var connection = new SqliteConnection(dbName);
-        connection.Open();
-        using (var command = connection.CreateCommand())
+        // Create a new database command
+        command = connection.CreateCommand();
+        // SQL query to display db values
+        command.CommandText = "SELECT * FROM " + tableName + ";";
+        // Execute the query
+        reader = command.ExecuteReader();
+        // Loop through results
+        while (reader.Read())
         {
-            command.CommandText = "Select name from game_stats;";
+            Debug.Log(
+                reader.GetInt32(0)
+                    + ", "
+                    + reader.GetString(1)
+                    + ", "
+                    + reader.GetInt32(2)
+                    + ", "
+                    + reader.GetInt32(3)
+            );
 
-            using (IDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Debug.Log(reader["name"]);
-                }
-            }
+            string result =
+                reader.GetInt32(0)
+                + "\t "
+                + reader.GetString(1)
+                + "\t\t"
+                + reader.GetInt32(2)
+                + "\t\t\t"
+                + reader.GetInt32(3);
+            textMeshPro.text = result;
+
         }
-        connection.Close();
+
+        // Clearing fields after use
+        reader.Close();
+        reader = null;
+        command.Dispose();
+        command = null;
+    }
+
+    private void InsertData()
+    {
+        // To do
+        // implement method to insert into db
+        // take values from parameters and pass it to the query to insert into table
     }
 }
