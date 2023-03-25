109bf6e7dc62b3a25e246496c38ce9f5ed679cd0
Joshua Uzell <G00391557@gmit.ie>
2023-03-25 12:11:34 +0000

    Fixed bugs with knockback effect
    Fixed issue where hitting the maul off of a group of enemies would make them stay white even though they should have turned back to red after a couple of seconds.

---
+++ U_PPIT Project/Assets/Scripts/SpriteFlashScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class SpriteFlashScript : MonoBehaviour
+{
+    [SerializeField] private float flashSpriteDelay = 2f;
+    [SerializeField] private string weaponTag = "";
+    private Health spriteHealth;
+
+    private void Awake()
+    {
+        spriteHealth = GetComponentInParent<Health>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(weaponTag) && spriteHealth.CurrentHealth > 0)
+        {
+            StartCoroutine(FlashSprite_Coroutine());
+        }
+    }
+
+    IEnumerator FlashSprite_Coroutine()
+    {
+        GetComponentInParent<SpriteRenderer>().color = Color.white;
+        yield return new WaitForSeconds(flashSpriteDelay);
+        GetComponentInParent<SpriteRenderer>().color = Color.red;
+    } 
+}
