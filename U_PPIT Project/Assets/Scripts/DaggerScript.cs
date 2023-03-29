0b7fac1dd0abcf514698a5adfd9731e9d97f1fda
Joshua Uzell <G00391557@gmit.ie>
2023-03-29 10:42:59 +0100

    Fixed the shooting dagger directions and added dagger sprite
    Fixed shooting for the dagger when the player looks/moves up,down left or right. Added a sprite for the dagger.

--- U_PPIT Project/Assets/Scripts/DaggerScript.cs
+++ U_PPIT Project/Assets/Scripts/DaggerScript.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DaggerScript : MonoBehaviour
@@ -10,13 +7,36 @@ public class DaggerScript : MonoBehaviour
     public float daggerSpeed;
     private Rigidbody2D spawnedDaggerRb;
     private bool hasFired = false;
-
+    [SerializeField] private GameObject directionHelper;
+    
     //Update is used for input from player
     private void Update()
     {
+        RotateDirectionHelper();
         if (Input.GetKeyDown(KeyCode.F))
         {
             hasFired = true;
+        }
+    }
+
+    private void RotateDirectionHelper()
+    {
+        //If left
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            directionHelper.transform.localRotation = Quaternion.Euler(0, 0f, -90f);
+        }
+        else if (Input.GetAxis("Horizontal") > 0) //If right
+        {
+            directionHelper.transform.localRotation = Quaternion.Euler(0, 0f, 90f);
+        }
+        else if (Input.GetAxis("Vertical") < 0) //If down
+        {
+            directionHelper.transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0) //if up
+        {
+            directionHelper.transform.localRotation = Quaternion.Euler(0, 0f, 180f);
         }
     }
 
@@ -27,9 +47,46 @@ public class DaggerScript : MonoBehaviour
         {
             var spawnedDagger = Instantiate(dagger, transform.position, Quaternion.identity);
             spawnedDaggerRb = spawnedDagger.GetComponent<Rigidbody2D>();
-            spawnedDaggerRb.velocity = Vector2.up * (daggerSpeed * Time.fixedDeltaTime);
+            
+            //If left
+            if (directionHelper.transform.localRotation == Quaternion.Euler(0f,0f,-90f))
+            {
+                spawnedDaggerRb.velocity = Vector2.left * (daggerSpeed * Time.fixedDeltaTime);
+                spawnedDagger.transform.rotation = Quaternion.Euler(0f, 0f, 90f);        
+            }
+            //If right
+            if (directionHelper.transform.localRotation == Quaternion.Euler(0f,0f,90f)) 
+            {
+                spawnedDaggerRb.velocity = Vector2.right * (daggerSpeed * Time.fixedDeltaTime);
+                spawnedDagger.transform.rotation = Quaternion.Euler(0f, 0f, -90f);        
+            }
+            
+            //If down
+            if (directionHelper.transform.localRotation == Quaternion.Euler(0f,0f,0f))
+            {
+                spawnedDaggerRb.velocity = Vector2.down * (daggerSpeed * Time.fixedDeltaTime);
+                spawnedDagger.transform.rotation = Quaternion.Euler(0f, 0f, 180f);        
+            }
+            
+            //If up
+            if (directionHelper.transform.localRotation == Quaternion.Euler(0f,0f,180f))
+            {
+                spawnedDaggerRb.velocity = Vector2.up * (daggerSpeed * Time.fixedDeltaTime);
+                spawnedDagger.transform.rotation = Quaternion.Euler(0f, 0f, 0f);        
+            }
+            
             Destroy(spawnedDagger, 4f);
             hasFired = false;
         }
     }
+    
+    //REFERENCES//
+    /***************************************************************************************
+    *    Title: How to make 2D Top Down Movement (Brackeys/Continued)(Unity Tutorial)
+    *    Author: JTA Games
+    *    Date: September 24, 2020
+    *    Code version: Unknown
+    *    Availability: https://www.youtube.com/watch?v=fRpoE4FfJf8&t=1s
+    *
+    ***************************************************************************************/
 }
