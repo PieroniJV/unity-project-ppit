e2cb96735e3e731806814001c458a7b3a5a0e48a
Joshua Uzell <G00391557@gmit.ie>
2023-04-04 12:12:06 +0100

    Added rotation animations to the bulkier zombies
    Added to the NormalEnemyReaction script with rotation animations that effect the bulkier zombies, based on where they are relative to the player.

--- U_PPIT Project/Assets/Scripts/NormalEnemyReactionScript.cs
+++ U_PPIT Project/Assets/Scripts/NormalEnemyReactionScript.cs
@@ -1,22 +1,66 @@
+using System;
 using UnityEngine;
 
 public class NormalEnemyReactionScript : MonoBehaviour
 {
     public float speed = 3f;
     [SerializeField] private Rigidbody2D enemyRb;
-    
+    [SerializeField] private Animator enemyAnimator;
+    private Vector3 distanceBetweenObjects;
+
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("ExitTrigger"))
+        if (other.CompareTag("PlayerCollider"))
         {
-           FollowPlayer(other);
+            FollowPlayer(other);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerCollider") && enemyAnimator != null)
+        {
+            enemyAnimator.speed = 0.3f;
+            enemyAnimator.SetBool("isRunning", false);
         }
     }
 
     private void FollowPlayer(Collider2D player)
     {
-        Vector3 distanceBetweenObjects = (player.transform.position - transform.parent.position).normalized;
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.speed = 1f;
+        }
+
+        //Code modified from The Code Anvil video - reference below
+        distanceBetweenObjects = (player.transform.position - transform.parent.position).normalized;
+        distanceBetweenObjects.Normalize();
+
+        if (enemyAnimator != null)
+        {
+            //Code modified from The Code Anvil video - reference below
+            enemyAnimator.SetFloat("Horizontal", distanceBetweenObjects.x);
+            enemyAnimator.SetFloat("Vertical", distanceBetweenObjects.y);
+        }
+
         enemyRb.AddForce(distanceBetweenObjects * speed);
+
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetBool("isRunning", true);
+        }
     }
-    
 }
+
+//////////////
+//REFERENCES//
+//////////////
+
+/***************************************************************************************
+*    Title: Unity top down Enemy AI and animation
+     Author: The Code Anvil
+     Date: Feb 1, 2021
+*    Code version: Unknown
+*    Availability: https://www.youtube.com/watch?v=2xaQYZW6LLA
+*
+***************************************************************************************/
