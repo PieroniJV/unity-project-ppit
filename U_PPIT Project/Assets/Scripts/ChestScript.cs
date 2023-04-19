69768a09bd503bb441c3bc6674b5aefbee6a7750
Joshua Uzell <G00391557@gmit.ie>
2023-04-19 21:44:14 +0100

    Set up gifts system for chests
    Set up gifts system where player sees the item they recieved from the chest

--- U_PPIT Project/Assets/Scripts/ChestScript.cs
+++ U_PPIT Project/Assets/Scripts/ChestScript.cs
@@ -1,15 +1,25 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class ChestScript : MonoBehaviour
 {
     private Animator chestAnimator;
     [SerializeField] private Animator panel;
+    [SerializeField] private GameObject giftImageGO;
+    [SerializeField] private TextMeshProUGUI giftText;
+    [SerializeField] private Sprite giftSprite;
+    [SerializeField] private String giftName;
+    [SerializeField] private float imageWidth = 0f;
+    
+    private Image giftImage;
     
     private void Awake()
     {
+        giftImage = giftImageGO.GetComponent<Image>();
         chestAnimator = GetComponent<Animator>();
     }
 
@@ -17,6 +27,7 @@ public class ChestScript : MonoBehaviour
     {
         if (other.CompareTag("PlayerCollider"))
         {
+            DisplayGift();
             chestAnimator.SetBool("isOpen", true);
             panel.SetBool("hasOpenedPanel", true);
         }
@@ -30,4 +41,25 @@ public class ChestScript : MonoBehaviour
             panel.SetBool("hasOpenedPanel", false);
         }
     }
+
+
+    //The code from the method below was modified from code found on Unity Answers, link to reference is at the bottom of this script
+    private void DisplayGift()
+    {
+        var giftImageRectTransform = giftImageGO.transform as RectTransform;
+        giftImageRectTransform.sizeDelta = new Vector2 (imageWidth, giftImageRectTransform.sizeDelta.y);
+        giftImage.sprite = giftSprite;
+        giftText.text = giftName;
+    }
 }
+
+//REFERENCES// 
+/***************************************************************************************
+*    Title: Change width of UI Image C#
+*    Question By: FlashX 
+*    Answer By: Kornikolia, Eudaimonium
+*    Date Asked: Sep 27, 2015
+     Date Answered: Sep 29, 2015
+*    Availability: https://answers.unity.com/questions/1072456/change-width-of-ui-image-c.html
+*
+***************************************************************************************/
